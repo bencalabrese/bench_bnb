556d8e8747d47b045e5b7d8172ba7f0f2d3c9317
Ben Calabrese <bencalabrese@gmail.com>
2016-04-25 10:38:45 -0700

    Setup React routes

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,34 @@
 var React = require('react'),
-    ReactDOM = require('react-dom'),
-    Search = require('./components/benches/search');
+    ReactDOM = require('react-dom');
+
+var ReactRouter = require('react-router'),
+    Router = ReactRouter.Router,
+    Route = ReactRouter.Route,
+    IndexRoute = ReactRouter.IndexRoute,
+    hashHistory = ReactRouter.hashHistory;
+
+var Search = require('./components/benches/search');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <header><h1>Bench BnB</h1></header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var appRouter = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Search}/>
+    </Route>
+  </Router>
+);
+
 
 $(function() {
-  ReactDOM.render(<Search/>, document.getElementById('content'));
+  ReactDOM.render(appRouter, document.getElementById('content'));
 });
